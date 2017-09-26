9bb515a0e8c4149c327e68bee75db1f717a1449c
Akshay Saini <anonyxhappie@gmail.com>
2017-09-26 09:48:54 +0530

    Initial Commit

---
+++ app/src/main/java/com/anonyxhappie/dwarf/pms2/DetailsActivity.java
@@ -0,0 +1,58 @@
+package com.anonyxhappie.dwarf.pms2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        final MovieModel movieModel = getIntent().getParcelableExtra("i_key");
+
+        TextView title = (TextView) findViewById(R.id.movie_title);
+        title.setText(movieModel.getOriginal_title());
+
+        ImageView poster = (ImageView) findViewById(R.id.poster);
+        Glide.with(this)
+                .load(Utils.generateImageUrl(movieModel.getPoster_path()))
+                .into(poster);
+        TextView overview = (TextView) findViewById(R.id.overview);
+        overview.setText(movieModel.getOverview());
+        TextView ratings = (TextView) findViewById(R.id.rating);
+        ratings.setText(String.valueOf(movieModel.getVote_average() ));
+        TextView date = (TextView) findViewById(R.id.year);
+        date.setText(movieModel.getRelease_date());
+
+        final Button favourite = (Button) findViewById(R.id.favourite);
+        if (movieModel.isFavourite()){
+            favourite.setText("Remove From Favourites");
+        }else{
+            favourite.setText("Mark As Favourite");
+        }
+
+        favourite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (movieModel.isFavourite()==false){
+                    movieModel.setFavourite(true);
+                    MainActivity.favouriteList.add(movieModel);
+                    favourite.setText("Remove From Favourites");
+                }else{
+                    movieModel.setFavourite(false);
+                    MainActivity.favouriteList.remove(movieModel);
+                    favourite.setText("Mark As Favourite");
+                }
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/anonyxhappie/dwarf/pms2/MainActivity.java
@@ -0,0 +1,134 @@
+package com.anonyxhappie.dwarf.pms2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String IMDBURL = "https://api.themoviedb.org/3/movie/";
+
+    public static String API = "?api_key="+ BuildConfig.THEMOVIEDB_ORG_API_KEY +"&language=en-US&page=1";
+
+    public static ArrayList<MovieModel> favouriteList = new ArrayList<>();
+
+    MovieAsyncTask movieAsyncTask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(isNetworkAvailable()){
+            movieAsyncTask = new MovieAsyncTask(this);
+            movieAsyncTask.execute(IMDBURL+"popular"+API);
+        }else {
+            Toast.makeText(this, "Please Connect to Internet.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(isNetworkAvailable()){
+            switch (item.getItemId()){
+                case R.id.item1:
+                    movieAsyncTask = new MovieAsyncTask(this);
+                    movieAsyncTask.execute(IMDBURL+"popular"+API);
+                    return true;
+                case R.id.item2:
+                    movieAsyncTask = new MovieAsyncTask(this);
+                    movieAsyncTask.execute(IMDBURL+"top_rated"+API);
+                    return true;
+//            case R.id.item3:
+//                movieAsyncTask.updateUi(favouriteList);
+//                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
+        }else {
+            Toast.makeText(this, "Please Connect to Internet.", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Function to check network state
+     * @return network state
+     */
+    private boolean isNetworkAvailable(){
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo info = manager.getActiveNetworkInfo();
+        return info != null && info.isConnected();
+    }
+
+    private class MovieAsyncTask extends AsyncTask<String, Void, ArrayList<MovieModel>> {
+
+        Context context;
+        GridView view;
+
+        private MovieAsyncTask(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected ArrayList<MovieModel> doInBackground(String... params) {
+
+            if (params.length < 1 || params[0]==null){
+                return null;
+            }
+            try {
+                 return  Utils.extractDataFromJSON(Utils.makeHttpRequest(Utils.generateUrl(params[0])));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<MovieModel> movieList) {
+            super.onPostExecute(movieList);
+            updateUi(movieList);
+        }
+
+        private void updateUi(final ArrayList<MovieModel> movieList){
+            view = (GridView) findViewById(R.id.grid_view);
+
+            GridAdapter adapter = new GridAdapter(context, movieList);
+            adapter.notifyDataSetChanged();
+            view.setAdapter(adapter);
+
+            view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent intent = new Intent(context, DetailsActivity.class);
+                    MovieModel movieModel = (MovieModel) parent.getAdapter().getItem(position);
+                    intent.putExtra("i_key", movieModel);
+                    startActivity(intent);
+                }
+            });
+        }
+
+    }
+}
