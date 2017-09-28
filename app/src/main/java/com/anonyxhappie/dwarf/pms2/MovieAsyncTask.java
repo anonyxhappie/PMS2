4035ba10976bd4f4361672c76bdf47e957166b18
Akshay Saini <anonyxhappie@gmail.com>
2017-09-28 10:07:55 +0530

    grd Changed to recyclerView and async class saperated from mainActivity

---
+++ app/src/main/java/com/anonyxhappie/dwarf/pms2/MovieAsyncTask.java
@@ -0,0 +1,44 @@
+package com.anonyxhappie.dwarf.pms2;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by dwarf on 9/28/2017.
+ */
+
+public class MovieAsyncTask extends AsyncTask<String, Void, ArrayList<MovieModel>> {
+
+    Context context;
+    AsyncTaskCompleteListener<ArrayList<MovieModel>> listener;
+
+    public MovieAsyncTask(Context context, AsyncTaskCompleteListener<ArrayList<MovieModel>> listener
+    ) {
+        this.context = context;
+        this.listener = listener;
+    }
+
+    @Override
+    protected ArrayList<MovieModel> doInBackground(String... params) {
+
+        if (params.length < 1 || params[0] == null) {
+            return null;
+        }
+        try {
+            return Utils.extractDataFromJSON(Utils.makeHttpRequest(Utils.generateUrl(params[0])));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(ArrayList<MovieModel> movieList) {
+        super.onPostExecute(movieList);
+        listener.onTaskComplete(movieList);
+    }
+
+}
